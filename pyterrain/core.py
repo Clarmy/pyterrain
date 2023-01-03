56d5df476d023a48e44acd23addff95ae52de015
Clarmy Lee <clarmylee92510@gmail.com>
2023-01-03 15:38:03 +0800

    feat: add clip and cache

--- pyterrain/core.py
+++ pyterrain/core.py
@@ -1,9 +1,13 @@
+import os
 import io
+import shutil
 from typing import List
 
 import requests
 import mercantile
 import numpy as np
+import pyproj
+
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -34,7 +38,7 @@ class Terrain:
             max_limit (int, optional): The maximum zoom level limit. Defaults to 6.
 
         Returns:
-            int: The suitable zoome.
+            int: The suitable zoom.
         """
         for zoom in range(15):
             left_upper_tile = mercantile.tile(left, upper, zoom)
@@ -53,7 +57,16 @@ class Terrain:
 
         return zoom
 
-    def fetch(self, bbox: List, zoom: int = None, progress_bar=False) -> np.ndarray:
+    def fetch(
+        self,
+        bbox: List,
+        zoom: int = None,
+        progress_bar=False,
+        timeout=10,
+        cache_path="./cache",
+        keep_cache=False,
+        coord="xy",
+    ) -> tuple:
         left, upper, right, lower = bbox
         if zoom is None:
             zoom = self._find_suitable_zoom(left, upper, right, lower)
@@ -66,6 +79,17 @@ class Terrain:
         tile_right_x = right_upper_tile.x
         tile_upper_y = left_upper_tile.y
         tile_lower_y = right_lower_tile.y
+
+        left_upper_bound = mercantile.bounds(left_upper_tile)
+        right_lower_bound = mercantile.bounds(right_lower_tile)
+
+        left_x, upper_y = mercantile.xy(left_upper_bound.west, left_upper_bound.north)
+        right_x, lower_y = mercantile.xy(
+            right_lower_bound.east, right_lower_bound.south
+        )
+
+        user_left_x, user_upper_y = mercantile.xy(*bbox[:2])
+        user_right_x, user_lower_y = mercantile.xy(*bbox[2:])
 
         urls = []
         for nx, x in enumerate(range(tile_left_x, tile_right_x + 1, 1)):
@@ -81,35 +105,73 @@ class Terrain:
             4,
         )
         canvas = np.full(shape, np.nan)
+        ys1d = np.linspace(upper_y, lower_y, shape[0])
+        xs1d = np.linspace(left_x, right_x, shape[1])
+
+        y_upper_diff = np.abs(ys1d - user_upper_y)
+        y_lower_diff = np.abs(ys1d - user_lower_y)
+        x_left_diff = np.abs(xs1d - user_left_x)
+        x_right_diff = np.abs(xs1d - user_right_x)
+
+        y_upper_i = np.where(y_upper_diff == y_upper_diff.min())[0][0]
+        y_lower_i = np.where(y_lower_diff == y_lower_diff.min())[0][0]
+        x_left_i = np.where(x_left_diff == x_left_diff.min())[0][0]
+        x_right_i = np.where(x_right_diff == x_right_diff.min())[0][0]
+
+        idx = np.s_[y_upper_i:y_lower_i, x_left_i:x_right_i]
+
+        ys = (np.full(shape[:2], 1) * ys1d[np.newaxis].T)[idx]
+        xs = (np.full(shape[:2], 1) * xs1d[np.newaxis])[idx]
 
         for (nx, ny), url in tqdm(urls, disable=not progress_bar):
-            resp = requests.get(url, timeout=7, stream=True)
-            if resp.ok:
-                buffer = io.BytesIO(resp.content)
+            tmpfp = os.path.join(cache_path, f"{zoom}_{nx}_{ny}.bin")
+            if os.path.exists(tmpfp):
+                with open(tmpfp, "rb") as f:
+                    img = (plt.imread(f) * 255).astype(int)
+            else:
+                resp = requests.get(url, timeout=timeout, stream=True)
+                if resp.ok:
+                    content = resp.content
+                buffer = io.BytesIO(content)
+                os.makedirs(os.path.dirname(tmpfp), exist_ok=True)
+                try:
+                    with open(tmpfp, "wb") as f:
+                        f.write(content)
+                except Exception:
+                    os.remove(tmpfp)
+
                 img = (plt.imread(buffer) * 255).astype(int)
-                canvas[ny * 256 : ny * 256 + 256, nx * 256 : nx * 256 + 256] = img
+
+            canvas[ny * 256 : ny * 256 + 256, nx * 256 : nx * 256 + 256] = img
 
         red = canvas[..., 0]
         green = canvas[..., 1]
         blue = canvas[..., 2]
-        elevation = ((red * 256 + green + blue / 256) - 32768).astype(int)
+        elevation = ((red * 256 + green + blue / 256) - 32768).astype(int)[idx]
 
-        return elevation
+        if not keep_cache:
+            shutil.rmtree(cache_path)
+
+        if coord == "lonlat":
+            proj = pyproj.Transformer.from_crs(3857, 4326, always_xy=True)
+            lons, lats = proj.transform(xs, ys)
+            return lons, lats, elevation
+        elif coord == "xy":
+            return xs, ys, elevation
 
 
 if __name__ == "__main__":
-    bbox = 76.98806, 37.454188, 103.2594, 19.70498
+    bbox = -108.994413, 40.944689, -102.00344, 37.011654
 
-    terrain = Terrain("Dto0r88DQuaQizoxcQScvw")
+    terrain = Terrain("qBD4m7PNT5apV-Xl7PROxA")
 
-    elevation = terrain.fetch(bbox=bbox, progress_bar=True)
+    xs, ys, elevation = terrain.fetch(bbox=bbox, progress_bar=True, coord="lonlat")
 
-    print(f"max of elevation: {elevation.max()}")
     fig = plt.figure(
         figsize=(elevation.shape[1] / 100, elevation.shape[0] / 100), dpi=100
     )
     ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
     ax.set_axis_off()
     fig.add_axes(ax)
-    ax.imshow(elevation, cmap=plt.cm.terrain)
-    fig.savefig("./tibat-plateau.png")
+    ax.imshow(elevation, cmap=plt.cm.terrain, vmin=-800)
+    fig.savefig("./foo.png")
