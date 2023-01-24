f32d2d24aa91a414ea30c6ebbbd3e0e43b38f022
Clarmy Lee <clarmylee92510@gmail.com>
2023-01-24 21:17:42 +0800

    chore: version

--- pyterrain/__init__.py
+++ pyterrain/__init__.py
@@ -1,1 +1,3 @@
 from .core import Terrain # noqa
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 import setuptools
 import os
+import codecs
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 with open(os.path.join(FILE_PATH, "README.md"), "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+    try:
+        long_description = fh.read()
+    except UnicodeDecodeError:
+        pass
 
 requirements_path = os.path.join(FILE_PATH, "requirements/base.txt")
 with open(requirements_path, encoding="utf-8") as f:
@@ -12,7 +32,7 @@ with open(requirements_path, encoding="utf-8") as f:
 
 setuptools.setup(
     name="pyterrain",
-    version="0.0.1",
+    version=get_version("pyterrain/__init__.py"),
     author="Wentao Li",
     author_email="",
     description="A Python package to fetch terrain data easily.",
